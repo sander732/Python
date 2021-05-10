5c9afcc34960b6ac88eed849136b5ed7bb197fc5
sander732 <eligiussander@gmail.com>
2021-05-10 17:27:12 +0200

    Update Oefenig3.py

--- Taak lists/Oefenig3.py
+++ Taak lists/Oefenig3.py
@@ -12,8 +12,9 @@ De index van het eerste element.
 Hoeveel keer het getal in getallenlijst voorkomt.
 """
 print("Oefening 3 : getallenzoeker")
+import random
 getallen = []
-
+nummers=[]
 vis= 0
 while True:
     while vis < 20:
@@ -21,4 +22,17 @@ while True:
         getallen.append(getal)
         vis += 1
     mcdoo=int(input("geef een getal tussen 1 en 40  "))
+    print(getallen)
+    if mcdoo == 0:
+        break
+    if mcdoo < 0 or mcdoo > 40:
+        print('error niet tussen 0 en 40')
+        continue
+    kip=0
+    while kip < 20:
+        test=getallen[kip]
+        if test == mcdoo:
+            nummers.append(test)
+        kip += 1
+    print(len(nummers))
     
