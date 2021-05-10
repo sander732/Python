33e52fa71b99dd560618df7184f1b3c5971a4c9c
sander732 <eligiussander@gmail.com>
2021-05-10 09:08:50 +0200

    ik verveeel me

---
+++ list.py
@@ -0,0 +1,20 @@
+animals=['cat','dog','monkey']
+print(animals)
+
+
+#maakt een lijst
+spam=['cat',"bat","rat",'kunt']
+
+#geeft nummer 1 als een gewoone varrable niet meer een lijst 
+henk=spam[-1]
+print(henk)
+
+
+#geeft nummer 1 en nummer 2
+sup=spam[1:3]
+print(sup)
+
+
+#geevt nummer 1 maar nog als een list 
+vis=spam[1:2] 
+print(vis)
